a7889e9652be50513b3991b71bd52e8c6f23aeeb
evute007 <e.daugirdaite@gmail.com>
2017-04-02 09:07:12 +0300

    Entities and UI (#14)
    * UI: new Welcome page, created firstMain.xhtml
    UI: new CSS, added new style to all pages
    UI: new picture, added page logo
    
    * UI: new Welcome page, created firstMain.xhtml
    UI: fixed deleted context-param part of web.xml
    
    * UI: new Welcome page, created firstMain.xhtml
    UI: fixed delated <context-param> part of web.xml
    
    * UI: new logo added.
    UI: added new comment, action function needed for activate btn
    
    * UI: Modified addNewStudent.xhtml., styled and added new fields
    UI: Styled CSS, added image style
    UI: Modified first Btn of firstMai.xhtml, adding path to addNewStudent.xhtml
    
    * UI: Modified CSS, added more style
    UI: Modifies addNewStudent and firstMain, fixing but with logo and btns.
    UI: Modified addNewStudent, fixed form filling properties (placeholder)
    
    * UI: modified addNewStudent: added new functions to btn's
    
     UI: added new pages, addAdditionalInformation addNesApplication, addParents, addQualifications
    
    * UI: added JSF template basic-template.xhtml
    UI: JSF template added to all pages
    
    * UI: fixed bug in firstMain.xhtml, buttons works correctly
    
    * Entities: created entity dateOfBirth
    UI: added value dateOfBirth to addNewStudent
    UI: modified addNewStudent, created a table with all students

--- src/main/java/lt/vtvpmc/ems/pw/entities/Student.java
+++ src/main/java/lt/vtvpmc/ems/pw/entities/Student.java
@@ -19,11 +19,14 @@ public class Student implements Serializable {
 
     private String lastName;
 
+    private long dateOfBirth;
+
     private String email;
 
-    public Student(String firstName, String lastName, String email ) {
+    public Student(String firstName, String lastName, long dateOfBirth, String email ) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
         this.email = email;
     }
 
@@ -52,6 +55,14 @@ public class Student implements Serializable {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public long getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(long dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
     }
 
     public String getEmail() {
